f8db52cb5cab7b3241d3bc30654ff116e4292488
Joost Flick <joostflick@gmail.com>
2019-02-11 13:12:01 +0100

    added week2 to continue working

--- week2/README.md
+++ week2/README.md
@@ -1,7 +1,40 @@
-# Week 2 - Design and Refactor 🛠
+# Week 1 - Hello API 🐒
 
-Doel: Breakdown maken van de web app. Routes en states toevoegen. Detailpagina renderen.
+Doel: Data ophalen uit een API en renderen in een overzichtspagina.
 
-[Opdrachten](https://drive.google.com/open?id=1GMDTdW3LycAYpZSFI6gk_lrKrx8-zLWrNh69aaVEH5Y)
+![screenshot](./screenshot.png)
 
-[Slides](https://drive.google.com/open?id=1IqQeu1m0dQiSC_KCvrn8eencAgtYe7X6qT-gm0n9Bmc)
+## Link to live demo
+
+[Demo](https://joostflick.github.io/web-app-from-scratch-18-19/week1/)
+
+## Wat doet deze app
+
+De app combineert een willekeurige Trump quote met een willekeurige foto van een hond. Ook kan er naar een lijst van honderden mogelijke beledigingen gekeken worden. Daarbij worden willekeurige namen gecombineerd met willekeurige beledigingen voor die persoon.
+
+## De gebruikte API's
+
+Trump quotes:
+https://api.whatdoestrumpthink.com/api/v1/quotes
+
+Beperkingen: Weinig data, alleen algemene quotes / gepersonaliseerde quotes
+
+Random dog images:
+https://dog.ceo/api/breeds/image/random/
+
+Beperkingen: Maximaal 50 random afbeeldingen opvragen
+
+Random users:
+https://randomuser.me/api/
+
+Voor de eerste pagina (quote + afbeelding) heb ik synchrone get requests gedaan voor de afbeelding en de quote. Voor de lijstweergave heb ik het wat schaalbaarder gemaakt door verschillende requests met Promises op te lossen.
+
+## Interactie
+
+Knop om een nieuwe quote + hondenfoto aan te vragen, knop voor text to speech bij de quotes.
+De gebruiker kan heen en weer navigeren tussen de lijst weergave en de quote generator.
+
+## Wishlist
+
+- templating engine
+- een use case voor deze applicatie
---
+++ week2/public/js/list.js
@@ -0,0 +1,97 @@
+const link = 'https://api.whatdoestrumpthink.com/api/v1/quotes'
+const linkNames = 'https://randomuser.me/api/?results=573'
+
+//promises example by Joost Faber https://codepen.io/joostf/pen/OQxpxx
+const loadInsults = new Promise(function(resolve, reject) {
+  const request = new XMLHttpRequest()
+  request.open('GET', link, true)
+
+  request.onload = () => {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      const data = JSON.parse(request.responseText)
+      resolve(data.messages.personalized)
+    } else {
+      // We reached our target server, but it returned an error
+      reject(error)
+    }
+  }
+
+  request.onerror = () => {
+    // There was a connection error of some sort
+  }
+
+  request.send()
+})
+
+const loadNames = new Promise(function(resolve, reject) {
+  const request = new XMLHttpRequest()
+  request.open('GET', linkNames, true)
+
+  request.onload = () => {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      const data = JSON.parse(request.responseText)
+      const randomNames = []
+      data.results.forEach(element => {
+        randomNames.push(element.name.first)
+      })
+      resolve(randomNames)
+    } else {
+      // We reached our target server, but it returned an error
+      reject(error)
+    }
+  }
+
+  request.onerror = () => {
+    // There was a connection error of some sort
+  }
+
+  request.send()
+})
+
+Promise.all([loadNames, loadInsults]).then(function(values) {
+  createTable(values)
+})
+
+function createTable(values) {
+  const names = values[0]
+  const insults = values[1]
+  console.log(names)
+  console.log(values)
+
+  document.getElementById('insult').innerHTML =
+    'Insulting ' + insults.length + ' people Donald Trump style'
+
+  // get the reference for the body
+  const body = document.getElementsByTagName('body')[0]
+
+  // creates a <table> element and a <tbody> element
+  const tbl = document.createElement('table')
+  const tblBody = document.createElement('tbody')
+  // creating all cells
+  for (let i = 0; i < insults.length; i++) {
+    // creates a table row
+    const row = document.createElement('tr')
+
+    // Create a <td> element and a text node, make the text
+    // node the contents of the <td>, and put the <td> at
+    // the end of the table row
+    const cell = document.createElement('td')
+    const cellText = document.createTextNode(
+      names[i].charAt(0).toUpperCase() + names[i].slice(1) + ' ' + insults[i]
+    )
+    cell.appendChild(cellText)
+    row.appendChild(cell)
+
+    // add the row to the end of the table body
+    tblBody.appendChild(row)
+  }
+
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody)
+  // appends <table> into <body>
+  body.appendChild(tbl)
+  // sets the border attribute of tbl to 2;
+  tbl.setAttribute('border', '2')
+}
