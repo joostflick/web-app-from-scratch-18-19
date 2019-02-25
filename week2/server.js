e52053cb00b49f02218d81e2f48d45253e6f48d8
Joost Flick <joostflick@gmail.com>
2019-02-25 12:35:06 +0100

    converted from functional to OOP

--- week2/server.js
+++ week2/server.js
@@ -1,138 +1,127 @@
-// the Fisher-Yates (aka Knuth) Shuffle https://github.com/Daplie/knuth-shuffle
-function shuffle(array) {
-  let currentIndex = array.length,
-    temporaryValue,
-    randomIndex
+Helpers = {
+  // the Fisher-Yates (aka Knuth) Shuffle https://github.com/Daplie/knuth-shuffle
+  shuffle: function(array) {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex -= 1
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex -= 1
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex]
-    array[currentIndex] = array[randomIndex]
-    array[randomIndex] = temporaryValue
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex]
+      array[currentIndex] = array[randomIndex]
+      array[randomIndex] = temporaryValue
+    }
+
+    return array
   }
-
-  return array
 }
 
-//promises example by Joost Faber https://codepen.io/joostf/pen/OQxpxx
-const loadInsults = new Promise(function(resolve, reject) {
-  const request = new XMLHttpRequest()
-  const link = 'https://api.whatdoestrumpthink.com/api/v1/quotes'
-  request.open('GET', link, true)
+API = {
+  loadInsults: new Promise(function(resolve, reject) {
+    const request = new XMLHttpRequest()
+    const link = 'https://api.whatdoestrumpthink.com/api/v1/quotes'
+    request.open('GET', link, true)
 
-  request.onload = () => {
-    if (request.status >= 200 && request.status < 400) {
-      // Success!
-      const data = JSON.parse(request.responseText)
-      resolve(data.messages.personalized)
-    } else {
-      // We reached our target server, but it returned an err
-      reject(error)
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        const data = JSON.parse(request.responseText)
+        resolve(data.messages.personalized)
+      } else {
+        // We reached our target server, but it returned an err
+        reject(error)
+      }
     }
-  }
 
-  request.onerror = () => {
-    // There was a connection error of some sort
-  }
-
-  request.send()
-})
-
-const loadNames = new Promise(function(resolve, reject) {
-  const request = new XMLHttpRequest()
-  const linkNames = 'https://randomuser.me/api/?results=573'
-  request.open('GET', linkNames, true)
-
-  request.onload = () => {
-    if (request.status >= 200 && request.status < 400) {
-      // Success!
-      const data = JSON.parse(request.responseText)
-      // const randomNames = []
-      // data.results.forEach(element => {
-      //   randomNames.push(element.name.first)
-      // })
-      const users = data.results.map(user => ({
-        name:
-          user.name.first.charAt(0).toUpperCase() + user.name.first.slice(1),
-        lastName:
-          user.name.last.charAt(0).toUpperCase() + user.name.last.slice(1),
-        title: user.name.title,
-        email: user.email,
-        cellphone: user.cell,
-        gender: user.gender,
-        picture: user.picture,
-        location: user.location
-      }))
-      resolve(users)
-    } else {
-      // We reached our target server, but it returned an error
-      reject(error)
+    request.onerror = () => {
+      // There was a connection error of some sort
     }
-  }
 
-  request.onerror = () => {
-    // There was a connection error of some sort
-  }
+    request.send()
+  }),
+  loadNames: new Promise(function(resolve, reject) {
+    const request = new XMLHttpRequest()
+    const linkNames = 'https://randomuser.me/api/?results=573'
+    request.open('GET', linkNames, true)
 
-  request.send()
-})
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        const data = JSON.parse(request.responseText)
+        // const randomNames = []
+        // data.results.forEach(element => {
+        //   randomNames.push(element.name.first)
+        // })
+        const users = data.results.map(user => ({
+          name:
+            user.name.first.charAt(0).toUpperCase() + user.name.first.slice(1),
+          lastName:
+            user.name.last.charAt(0).toUpperCase() + user.name.last.slice(1),
+          title: user.name.title,
+          email: user.email,
+          cellphone: user.cell,
+          gender: user.gender,
+          picture: user.picture,
+          location: user.location
+        }))
+        resolve(users)
+      } else {
+        // We reached our target server, but it returned an error
+        reject(error)
+      }
+    }
 
-Promise.all([loadNames, loadInsults]).then(function(values) {
+    request.onerror = () => {
+      // There was a connection error of some sort
+    }
+
+    request.send()
+  })
+}
+
+Promise.all([API.loadNames, API.loadInsults]).then(function(values) {
   routie('insultlist', () => {
-    drawDom(values)
+    Render.drawList(values)
   })
   routie(':id', id => {
-    drawDetailPage(values[0][id])
+    Render.drawDetail(values[0][id])
   })
 })
 
-//Martijn was a huge inspiration for me here
-
-// no innerhtml
-function drawDetailPage(user) {
-  const element = document.getElementById('list')
-  element.innerHTML = `<div class="user">
+Render = {
+  drawDetail: function(user) {
+    const element = document.getElementById('list')
+    element.innerHTML = `<div class="user">
    <a href="#insultlist">Back to list</a>
         <img class="profilepic" src=${user.picture.large}></img>
       <p>${user.name} ${user.lastName}</p>
       <p>${user.insult}</p>
       <p>Phone number: <a href="tel:${user.cellphone}"> ${
-    user.cellphone
-  }</a></p>
+      user.cellphone
+    }</a></p>
       <p>Directly insult this user via email: </p>
       <a href="mailto:${user.email}?subject=${user.name} ${user.insult}">${
-    user.email
-  }</a>
+      user.email
+    }</a>
       </div>
       `
-  console.log(user.location.coordinates)
-  drawMap(user.location.coordinates)
-}
-
-function drawMap(location) {
-  console.log(location)
-  map = document.getElementById('list')
-  map.innerHTML += `
-  <p>Find this person to tell him/her in person: </p>
-<div class="map" id="map"></div>
-`
-  initMap(location)
-}
-
-function drawDom(data) {
-  const element = document.getElementById('list')
-  const names = data[0]
-  const insults = shuffle(data[1])
-  for (let i = 0; i < insults.length; i++) {
-    names[i].insult = insults[i]
-    names[i].id = i
-  }
-  element.innerHTML = `
+    console.log(user.location.coordinates)
+    this.drawMap(user.location.coordinates)
+  },
+  drawList: function(data) {
+    const element = document.getElementById('list')
+    const names = data[0]
+    const insults = Helpers.shuffle(data[1])
+    for (let i = 0; i < insults.length; i++) {
+      names[i].insult = insults[i]
+      names[i].id = i
+    }
+    element.innerHTML = `
   <a href="./index.html">Give me some other insults!</a>
   ${names
     .map(item =>
@@ -146,6 +135,16 @@ function drawDom(data) {
   `.trim()
     )
     .join('')}`
+  },
+  drawMap: function(location) {
+    console.log(location)
+    map = document.getElementById('list')
+    map.innerHTML += `
+  <p>Find this person to tell him/her in person: </p>
+<div class="map" id="map"></div>
+`
+    initMap(location)
+  }
 }
 
 routie('insultlist')
